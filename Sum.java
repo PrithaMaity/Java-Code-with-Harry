a4ad842d6914d55ef5fd509dd85a9b9b4ee73b00
pritha <prithamaity2001@gmail.com>
2022-11-12 22:17:23 +0530

    Initial commit

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("sum of " + a + " and " + b + " is " + (a+b));
+        System.out.println("sum of " + a + " and " + b + " is :" + (a + b));
     }
 }
